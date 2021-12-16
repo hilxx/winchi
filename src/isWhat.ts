b4f849474882043bd332b126a58eba2b2f8e7fd4
thisislx <thisixlx@outllok.com>
2021-12-16 10:43:14 +0800

    dev: 新增部分函数和jsx函数

--- src/isWhat.ts
+++ src/isWhat.ts
@@ -1,9 +1,13 @@
-import { AO } from '.'
+import { AO } from '.';
 
-export const isObj = (o): o is AO => Reflect.apply(Object.prototype.toString, o, []) === '[object Object]'
+export const isExtendObj = (o): o is AO =>
+  (!!o && typeof o === 'object') || typeof o === 'function';
 
-export const isEmptyObj = (o: AO): o is AO => Reflect.ownKeys(o).length === 0
+export const isObj = (o): o is AO =>
+  Reflect.apply(Object.prototype.toString, o, []) === '[object Object]';
 
-export const isRichObj = (o: AO): o is AO => Reflect.ownKeys(o).length > 0
+export const isEmptyObj = (o: AO): o is AO => Reflect.ownKeys(o).length === 0;
 
-export const isEmpty = v => v === undefined || v === null || v === NaN
+export const isRichObj = (o: AO): o is AO => Reflect.ownKeys(o).length > 0;
+
+export const isEmpty = (v) => v === undefined || v === null || v === NaN;
